7cbc591c685065bc5d1e4f53d64b2c0fe017b66a
Hynek Fišera <53520774+hynekfisera@users.noreply.github.com>
2021-06-12 11:04:04 +0200

    Get request with URL params

--- index.js
+++ index.js
@@ -5,10 +5,20 @@ const members = require("./Members");
 
 const app = express();
 
-app.use(logger);
+//app.use(logger);
 
 app.get("/api/members", (req, res) => {
   res.json(members);
+});
+
+app.get("/api/members/:id", (req, res) => {
+  const found = members.some((member) => member.id === parseInt(req.params.id));
+
+  if (found) {
+    res.json(members.filter((member) => member.id === parseInt(req.params.id)));
+  } else {
+    res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
+  }
 });
 
 app.use(express.static(path.join(__dirname, "public")));
