ca47f6c78b904d5a9a981e53c7e74d85890537f8
Hynek Fišera <53520774+hynekfisera@users.noreply.github.com>
2021-06-12 10:57:09 +0200

    Middleware

---
+++ Members.js
@@ -0,0 +1,16 @@
+const members = [
+  {
+    id: 1,
+    name: "Member 1",
+  },
+  {
+    id: 2,
+    name: "Member 2",
+  },
+  {
+    id: 3,
+    name: "Member 3",
+  },
+];
+
+module.exports = members;
--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const path = require("path");
+const logger = require("./middleware/logger");
+const members = require("./Members");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello World</h1>");
+app.use(logger);
+
+app.get("/api/members", (req, res) => {
+  res.json(members);
 });
+
+app.use(express.static(path.join(__dirname, "public")));
 
 const PORT = process.env.PORT || 5000;
 
