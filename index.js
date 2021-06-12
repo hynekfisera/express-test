ec2f05cd6c440cad0800c739e9319c42aa2b7d15
Hynek Fišera <53520774+hynekfisera@users.noreply.github.com>
2021-06-12 11:16:35 +0200

    Router

--- index.js
+++ index.js
@@ -1,27 +1,14 @@
 const express = require("express");
 const path = require("path");
 const logger = require("./middleware/logger");
-const members = require("./Members");
 
 const app = express();
 
 //app.use(logger);
 
-app.get("/api/members", (req, res) => {
-  res.json(members);
-});
-
-app.get("/api/members/:id", (req, res) => {
-  const found = members.some((member) => member.id === parseInt(req.params.id));
-
-  if (found) {
-    res.json(members.filter((member) => member.id === parseInt(req.params.id)));
-  } else {
-    res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
-  }
-});
-
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/api/members", require("./routes/api/members"));
 
 const PORT = process.env.PORT || 5000;
 
---
+++ routes/api/members.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+const members = require("../../Members");
+
+router.get("/", (req, res) => {
+  res.json(members);
+});
+
+router.get("/:id", (req, res) => {
+  const found = members.some((member) => member.id === parseInt(req.params.id));
+
+  if (found) {
+    res.json(members.filter((member) => member.id === parseInt(req.params.id)));
+  } else {
+    res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
+  }
+});
+
+router.post("/", (req, res) => {
+  res.send(req.body);
+});
+
+module.exports = router;
